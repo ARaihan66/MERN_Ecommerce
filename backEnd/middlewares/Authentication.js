acd101092f447e5b2ad6d340ea076b14064cbf6b
Shakil <aburaihancse066@gmail.com>
2022-10-02 19:01:32 +0600

    add admin authorization

--- backEnd/middlewares/Authentication.js
+++ backEnd/middlewares/Authentication.js
@@ -13,6 +13,18 @@ exports.authentication = async (req, res, next) => {
     const verification = jwt.verify(token, process.env.JWT_SECRET_KEY);
 
     req.user = await User.findById(verification.id);
-
     next();
+}
+
+
+//admin role
+exports.authorizeRole = (...roles) => {
+    return (req, res, next) => {
+        if (!roles.includes(req.user.role)) {
+            res.status(400).json({
+                message: "User unable to access the resources!!!",
+            })
+        }
+        next();
+    }
 }
--- backEnd/route/ProductRoute.js
+++ backEnd/route/ProductRoute.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const { getAllProducts, createProduct, updateProduct, deleteProduct, getSingleProduct } = require('../middlewares/ProductController.js');
-const { authentication } = require('../middlewares/Authentication');
+const { authentication, authorizeRole } = require('../middlewares/Authentication');
 const router = express.Router();
 
 router.route('/products').get(getAllProducts);
-router.route('/product/new').post(authentication, createProduct);
+router.route('/product/new').post(authentication, authorizeRole("admin"), createProduct);
 router.route('/product/:id').put(updateProduct).delete(deleteProduct).get(getSingleProduct);
 
 module.exports = router;
