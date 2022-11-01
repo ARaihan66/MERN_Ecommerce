c2421b8577a10b82f1bb2074e7dd5a411270c772
Shakil <aburaihancse066@gmail.com>
2022-11-02 02:14:13 +0600

    Add some part of redux

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,8 +1,17 @@
 import './App.css';
 import Body from './components/Home/Body/Body';
 import Navigation from './components/Home/Header/Navbar';
+import { useEffect } from 'react';
+import webFont from 'webfontloader';
 
 function App() {
+  useEffect(() => {
+    webFont.load({
+      google: {
+        families: ['Roboto', 'Droid Sans', 'Chilanka'],
+      },
+    })
+  }, [])
   return (
     <div className="App">
       <Navigation />
