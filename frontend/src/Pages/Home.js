cc30cb1a90cd817ce59b0d274a39d5bd4f046d03
Shakil <aburaihancse066@gmail.com>
2022-11-04 00:12:01 +0600

    add slidder

--- frontend/src/Pages/Home.js
+++ frontend/src/Pages/Home.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import Announcement from '../components/Announcement'
 import Navbar from '../components/Navbar'
+import Slidder from '../components/Slidder'
 
 const Home = () => {
     return (
         <div>
             <Announcement />
             <Navbar />
+            <Slidder />
         </div>
     )
 }
---
+++ frontend/src/components/Data.js
@@ -0,0 +1,26 @@
+import Slidder1 from "../Picture/Slidder/Slidder1.png";
+import Slidder2 from "../Picture/Slidder/Slidder2.png";
+import Slidder3 from "../Picture/Slidder/Slidder3.png";
+export const SlidderItems = [
+    {
+        id: 1,
+        img: Slidder1,
+        title: "WINTER SALE",
+        description: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS",
+        bg: "f5fafd"
+    },
+    {
+        id: 2,
+        img: Slidder2,
+        title: "POPULER SALE",
+        description: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS",
+        bg: "f5f1ed"
+    },
+    {
+        id: 3,
+        img: Slidder3,
+        title: "NEW ARRIVAL",
+        description: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS",
+        bg: "f5f0f4"
+    }
+]
