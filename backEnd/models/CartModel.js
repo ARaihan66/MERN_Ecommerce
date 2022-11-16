799225c5b44eac0c1179cf93c98c6d63db1e2df9
Shakil <aburaihancse066@gmail.com>
2022-11-16 10:03:28 +0600

    change cart and products model

--- backEnd/models/CartModel.js
+++ backEnd/models/CartModel.js
@@ -1,34 +1,21 @@
 const { Schema, model } = require("mongoose");
 
 const cartSchema = new Schema({
-    productName: {
-        type: String,
-        required: [true, "Please enter your product name"],
-    },
-    productPrice: {
-        type: Number,
-        required: [true, "Please enter your product price"],
-    },
-    productImage: {
-        type: String,
-        required: [true, "Please enter your product image"],
-    },
-    quantity: {
-        type: Number,
-        required: [true, "Please enter your product quantity"],
-    },
     userId: {
         type: String,
-        required: [true, "Please enter your user id"],
+        required: true
     },
-    productId: {
-        type: String,
-        required: [true, "Please enter your user id"],
-    },
-    Stock: {
-        type: Number,
-        required: [true, "Please enter your product stock"],
-    }
+    products: [
+        {
+            productId: {
+                type: String
+            },
+            quantity: {
+                type: Number,
+                default: 1
+            }
+        }
+    ],
 }, { timestamps: true });
 
 module.exports = model("Cart", cartSchema);
